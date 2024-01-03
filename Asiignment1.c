80ebd5e8de47ed761380fc0f19cafb9cc200a338
hemant228 <155377644+hemant228@users.noreply.github.com>
2024-01-03 10:24:34 +0530

    Add files via upload
    These are the some question and solve the question by using C Program.

---
+++ Asiignment1.c
@@ -0,0 +1,23 @@
+  //(1) Why do computers understand only binary language?
+    /* Ans:- Because any hardware consist some material which made in 1 0r 0 which is binary.
+    */
+   //(2) What is the full form of IDE?
+   /* Ans:- Integrated Development Enviroment
+   */
+   // 3. What is the difference between a text editor and a code editor?
+     /* Ans:- In text editor we write the code and build in CMD or other platform. But in code
+              Editor we write and build the code in one place.
+     */
+    // 4. What are the steps to develop software using the C language?
+      /* 1st step:- Preprocessor
+      2nd step:- Compiler
+      3rd step:- Linker
+      */
+     // (5)a. What is the latest version of C Language?
+        /*
+        Ans:- C17
+        */
+       //b. Who developed C Language?
+       /*
+        Ans:- Dannis Ritche
+       */
